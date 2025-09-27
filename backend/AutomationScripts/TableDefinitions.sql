f7ac6aac4a3c55ed72a7421f433013c6077fbefc
Tanvir Jawad <tanvirj.the.artist@gmail.com>
2025-09-27 19:03:43 -0400

    Update #1

---
+++ backend/AutomationScripts/TableDefinitions.sql
@@ -0,0 +1,30 @@
+CREATE TYPE IF NOT EXISTS musicalkey AS ENUM(
+    'C', 'C#', 'Db', 'D', 'D#', 'Eb', 'E', 'F', 
+    'F#', 'Gb', 'G', 'G#', 'Ab', 'A', 'A#', 'Bb', 'B',
+    'Cm', 'C#m', 'Dbm', 'Dm', 'D#m', 'Ebm', 'Em', 'Fm',
+    'F#m', 'Gbm', 'Gm', 'G#m', 'Abm', 'Am', 'A#m', 'Bbm', 'Bm'
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS samples (
+    sample_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    bpm INTEGER,
+    sample_name VARCHAR(255) NOT NULL,
+    sample_url VARCHAR(500),
+    musical_key musicalkey,
+    tags VARCHAR(50)[],
+    uploader_id UUID REFERENCES users(user_id) ON DELETE SET NULL,
+    upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS saved_samples (
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    sample_id UUID REFERENCES samples(sample_id) ON DELETE CASCADE,
+    save_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (user_id, sample_id)
+);
---
+++ backend/app/main.py
@@ -0,0 +1,48 @@
+from typing import Union
+from fastapi import FastAPI, UploadFile
+from pydantic import BaseModel
+import s3_utils
+
+class Sample(BaseModel):
+    id: str
+    name: str
+    sample_url: str
+    ext: str
+    music_key: Union[str, None] = None
+    bpm: Union[int, None] = None
+    tags: List[str]
+
+
+app = FastAPI()
+
+
+# Get all the samples in the database and return an array of sample_ids
+@app.get("/samples/")
+def getAllSamples():
+    return {}
+
+# Get a sample by its sample_id
+@app.get("/samples/{sample_id}")
+def getSample(sample_id: str):
+    return {"sample": sample_id}
+
+# Upload a sample
+@app.post("/samples/")
+async def uploadSample(sampleFile: UploadFile):
+    uuid = ""
+    s3Link = s3_utils.s3.upload_fileobj(sampleFile.file, sampleFile.filename, uuid)
+    # TODO: Update the database
+    return {
+        "Success": "true",
+        "s3_link": s3Link
+    }
+
+# Get all the samples a user has saved
+@app.get("/user/saves/")
+def getSavedSamples(user_id: str):
+    return {}
+
+# Save a samples for a user
+@app.post("/user/saves/")
+def saveSample(user_id: str, sample_id: str):
+    return {}
